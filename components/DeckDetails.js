01a5669dc500722f899f47b4687b098423827105
Taryn Sauer <taryn.sauer@gmail.com>
2018-10-26 16:54:58 -0500

    feat: improve list animation

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Button, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { withNavigation } from 'react-navigation'
 
 const styles = StyleSheet.create({
   flatview: {
     justifyContent: 'center',
     paddingTop: 30,
-    borderRadius: 2,
   },
   title: {
     fontFamily: 'Verdana',
@@ -18,15 +17,16 @@ const styles = StyleSheet.create({
 })
 
 class DeckDetails extends React.Component {
+
   render() {
     const { questions, navigation, title } = this.props
 
-    return (<TouchableHighlight style={styles.flatview} onPress={() => navigation.navigate('Deck', {title: title})}>
+    return (<TouchableOpacity style={styles.flatview} onPress={()=> navigation.navigate('Deck', {title: title})}>
       <View>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.count}>Cards: {questions.length}</Text>
       </View>
-    </TouchableHighlight>
+    </TouchableOpacity>
     )
   }
 }
